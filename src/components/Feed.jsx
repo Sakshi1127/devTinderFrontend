fa3ccd2d5247d9b33e69624b117a4cc4f045ff82
Sakshi <sakshimaheshwari19191@gmail.com>
2026-02-22 17:27:22 +0530

    Create the feed page.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import {useEffect} from 'react'
+import UserCard from  './UserCard'
+import { BASE_URL } from '../utils/constants'
+import axios from 'axios'
+import { useDispatch,useSelector } from 'react-redux'
+import {addFeed } from '../utils/feedSlice'
 
 const Feed = () => {
-  return (
-    <div>
-      Feed
+  const dispatch = useDispatch();
+  const feed = useSelector((store)=>store.feed);
+  const fetchFeed= async()=>{
+    try{
+      const res = await axios.get(BASE_URL + '/feed',{withCredentials:true});
+      dispatch(addFeed(res.data))
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  useEffect(()=>{
+    fetchFeed();
+  },[])
+
+  return feed &&(
+    <div className='flex justify-center my-20'>
+      <UserCard feed={feed[0]}/>
     </div>
   )
 }
